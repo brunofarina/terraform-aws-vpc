8b67f45b8cceab8a4aca85d2ff43b39ad54c8dc9
Bruno Farina <bruno.ffarina@gmail.com>
2022-03-22 17:33:19 -0300

    Update main.tf to use data for azs names
    I was looking on the vpc azs' property and I found out how to use aws_availability_zones names on the data source instead of using ["${local.region}a", "${local.region}b", "${local.region}c"].

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -6,6 +6,10 @@ locals {
   region = "eu-west-1"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 ################################################################################
 # VPC Module
 ################################################################################
@@ -16,7 +20,7 @@ module "vpc" {
   name = "simple-example"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
